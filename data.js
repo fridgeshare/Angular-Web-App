3f2b422c7e4a5c75e6e2f9415e270cd8b3a3934e
Ramiro Gonzalez <ramirogonzalez@ocf.berkeley.edu>
2019-08-11 16:24:20 -0700

    Homepage and added icons using font-awesome

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = function(){
+    return{
+        products: [
+            { id: 1, name: "Stonyfield Organic Whole Milk Greek – Strawberry", category: "Yogurt",
+            description: "This thick, tangy yogurt is made by straining out most of the liquid whey. It’s higher in protein (nearly double!) and lower in sugar than conventional varieties, but it also generally has less calcium.", price: 1 },
+            { id: 2, name: "Stonyfield Organic Whole Milk Greek – Strawberry", category: "Yogurt",
+            description: "This thick, tangy yogurt is made by straining out most of the liquid whey. It’s higher in protein (nearly double!) and lower in sugar than conventional varieties, but it also generally has less calcium.", price: 1 },
+            { id: 3, name: "Stonyfield Organic Whole Milk Greek – Strawberry", category: "Yogurt",
+            description: "This thick, tangy yogurt is made by straining out most of the liquid whey. It’s higher in protein (nearly double!) and lower in sugar than conventional varieties, but it also generally has less calcium.", price: 1 },
+            { id: 4, name: "Stonyfield Organic Whole Milk Greek – Strawberry", category: "Yogurt",
+            description: "This thick, tangy yogurt is made by straining out most of the liquid whey. It’s higher in protein (nearly double!) and lower in sugar than conventional varieties, but it also generally has less calcium.", price: 1 },
+            { id: 5, name: "Stonyfield Organic Whole Milk Greek – Strawberry", category: "Yogurt",
+            description: "This thick, tangy yogurt is made by straining out most of the liquid whey. It’s higher in protein (nearly double!) and lower in sugar than conventional varieties, but it also generally has less calcium.", price: 1 },
+            { id: 6, name: "Stonyfield Organic Whole Milk Greek – Strawberry", category: "Yogurt",
+            description: "This thick, tangy yogurt is made by straining out most of the liquid whey. It’s higher in protein (nearly double!) and lower in sugar than conventional varieties, but it also generally has less calcium.", price: 1 },
+            { id: 7, name: "Stonyfield Organic Whole Milk Greek – Strawberry", category: "Yogurt",
+            description: "This thick, tangy yogurt is made by straining out most of the liquid whey. It’s higher in protein (nearly double!) and lower in sugar than conventional varieties, but it also generally has less calcium.", price: 1 },
+            { id: 8, name: "Stonyfield Organic Whole Milk Greek – Strawberry", category: "Yogurt",
+            description: "This thick, tangy yogurt is made by straining out most of the liquid whey. It’s higher in protein (nearly double!) and lower in sugar than conventional varieties, but it also generally has less calcium.", price: 1 },
+            { id: 9, name: "Stonyfield Organic Whole Milk Greek – Strawberry", category: "Yogurt",
+            description: "This thick, tangy yogurt is made by straining out most of the liquid whey. It’s higher in protein (nearly double!) and lower in sugar than conventional varieties, but it also generally has less calcium.", price: 1 }
+        ],
+        orders: []
+    }
+}
